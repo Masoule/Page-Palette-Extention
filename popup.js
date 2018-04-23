28694bbf6b6ce76c2c092d724758a458d29cd41b
salome siavoshi <salomei@gmail.com>
2018-04-22 22:10:40 -0700

    adds stylesheet

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var colorButton = document.getElementById('p-color');
   colorButton.addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
+      
     });
   }, false);
 }, false);
